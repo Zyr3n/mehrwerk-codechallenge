e9021f5553a508058d70fd118fa3d21286ee1642
Zyr3n <41127154+Zyr3n@users.noreply.github.com>
2023-12-21 10:46:04 +0100

    Initial commit

---
+++ src/main/java/de/jsiemssen/mehrwerkcodechallenge/MehrwerkCodechallengeApplication.java
@@ -0,0 +1,13 @@
+package de.jsiemssen.mehrwerkcodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehrwerkCodechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehrwerkCodechallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jsiemssen/mehrwerkcodechallenge/MehrwerkCodechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jsiemssen.mehrwerkcodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MehrwerkCodechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
